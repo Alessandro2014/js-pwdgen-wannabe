d93a1cb4da230f1a491050cb6bb8e3e4c7d83481
Alessandro2014 <alessandromerenda22@gmail.com>
2021-06-30 16:48:37 +0200

    analisi step da effettuare

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+1 creare variabile richiesta nome
+2 creare variabile richiesta cognome
+3 creare variabile richiesta colore preferito
+4 creare variabile per numerto 21
+5 prendere riferimento id password
+6 inserire risultato in HTML
+*/
