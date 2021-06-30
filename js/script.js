0eb610a766462ff4717f13397b8b8d1675037b74
Alessandro2014 <alessandromerenda22@gmail.com>
2021-06-30 17:31:06 +0200

    conclusione

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@
 5 prendere riferimento id password
 6 inserire risultato in HTML
 */
+
+// VARIABILI
+var message = "La tua Password è: "
+var passwordPlaceholder = document.getElementById("password");
+var name = prompt("Inserisci il tuo nome");
+var lastName = prompt("Inserisci il tuo cognome");
+var color = prompt("Inserisci il tuo colore preferito");
+var number = 21;
+
+//CALCOLO PASSWORD
+
+var yourPassword = name + lastName + color + number;
+console.log("password utente: ", yourPassword);
+
+//MOSTRARE PASSWORD ALL'UTENTE
+
+passwordPlaceholder.innerHTML = message + yourPassword;
